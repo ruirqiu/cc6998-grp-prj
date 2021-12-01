ec1ec4a4bbe0b98358a78580ab9bd9f4d5ea3903
claire-qw <lwang5829@gmail.com>
2021-11-30 22:31:13 -0500

    modified search bar

---
+++ frontend/src/components/Search/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import './Search.css'
+
+function SearchBar () {
+
+  const [query, setQuery] = useState("")
+
+  const onChange = e => {
+    setQuery(e.target.value)
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(query)
+  }
+
+  return (
+    <Paper
+      component="form"
+      className='searchBarContainer'
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+    >
+      <InputBase
+      onChange={onChange}
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Product Name"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <Divider sx={{ height: 36, m: 0.5 }} orientation="vertical" />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={onSubmit}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
+
+export default SearchBar;
