92976986ebda920e5316a50fecb4fc118cf6ac89
yumeng-luo <claire.luo414@gmail.com>
2021-12-03 23:06:53 -0500

    Polish route text details to inlcude summary, items purchased

--- frontend/src/components/Route/Route.js
+++ frontend/src/components/Route/Route.js
@@ -10,21 +10,35 @@ import ListItemText from '@mui/material/ListItemText';
 import './Route.css'
 
 function Route({ routeItems }) {
-    
+  var summary_text = "Total Price: "+routeItems.total_price_dollar+"    | Total Distance: "+routeItems.total_distance_mile + "     | Total Travel Time: "+ routeItems.total_duration_min
+
   return (
     <>
+    <div>{summary_text}</div>  
     <Box className='routeResult'>
        <nav aria-label="secondary mailbox folders">
           <List>
              {routeItems.route_details.map(function (d, idx) {
+              var idx_text="Stop "+(idx+1)+":     "+ (d.brand_name);
+              var name_text="Store Location: "+ (d.store_name);
+              var geo_text="Latittude: "+ (d.lat) + "     Longitutde: "+ (d.lon);
              return (
              <React.Fragment key={`listitem-${idx}`}>
                 <ListItem disablePadding>
                    <ListItemButton style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                   <ListItemText style={{ marginRight: '50px' }} primary={d.brand_name} />
-                   <ListItemText primary={d.store_name} />
-                   <ListItemText primary={d.lat} />
-                   <ListItemText primary={d.lon} />
+                   <ListItemText style={{ marginRight: '50px' }} primary={idx_text} />
+                   <ListItemText secondary={name_text} />
+                   <ListItemText secondary={geo_text} />
+                   <ListItemText primary="The following items are purchased here:" />
+                     {d.purchased_here.map(function (i, idx_i) {
+                        var idx_i_text="Item "+(idx_i+1)+":    "+ (i.item_id)+ "    $"+i.price;
+                        return (
+                        <React.Fragment key={`listitem-${idx_i}`}>
+                                           <small> {idx_i_text} </small>
+                                        <Divider />
+                                     </React.Fragment>
+                                     )
+                                     })}
                    </ListItemButton>
                 </ListItem>
                 <Divider />
